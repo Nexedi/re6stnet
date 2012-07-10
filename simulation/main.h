1c0c9b54963381fbf5d8a5d8fa8e105eeef7e1b4
Ulysse Beaugnon <ulysse.beaugnon@tiolive.com>
2012-07-10 18:47:44 +0900

    Creation of a simulator to find out how our network behave on a large scale environment

---
+++ simulation/main.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <queue>
+#include <set>
+
+#define max(a,b) a>b?a:b
+#define min(a,b) a<b?a:b
+#define clamp(a,b,c) max(a,min(b, c))
+#define array(name, size) name[size]; for(int i=0; i<size; i++) name[i]=0;
+
+using namespace std;
+
+class Graph
+{
+public:
+    Graph(int size, int k, int maxPeers, mt19937 rng);
+    ~Graph() { delete[] adjacency; };
+
+    void GetDistancesFrom(int node, int* distance);
+    //void KillMachines(float portion, vector<int>* graph)
+   
+    vector<int>* adjacency;
+    int size;
+private:
+    uniform_int_distribution<int> distrib;
+};
+
