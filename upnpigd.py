7d4346a91d5cb9faffd5b2450de9211dbfec4e6e
Julien Muchembled <jm@nexedi.com>
2012-07-18 10:44:38 +0200

    Remove \t from upnpigd.py

--- upnpigd.py
+++ upnpigd.py
@@ -3,17 +3,17 @@ import socket
 
 # return (address, port)
 def ForwardViaUPnP(localPort):
-	u = miniupnpc.UPnP()
-	u.discoverdelay = 200
-	u.discover()
-	u.selectigd()
-	externalPort = 1194
-	while True:
-		while u.getspecificportmapping(externalPort, 'UDP') != None:
-			externalPort = max(externalPort + 1, 49152)
-			if externalPort == 65536:
-				raise Exception
-		if u.addportmapping(externalPort, 'UDP', u.lanaddr, localPort, 'Vifib openvpn server', ''):
-			return (u.externalipaddress(), externalPort)
+    u = miniupnpc.UPnP()
+    u.discoverdelay = 200
+    u.discover()
+    u.selectigd()
+    externalPort = 1194
+    while True:
+        while u.getspecificportmapping(externalPort, 'UDP') != None:
+            externalPort = max(externalPort + 1, 49152)
+            if externalPort == 65536:
+                raise Exception
+        if u.addportmapping(externalPort, 'UDP', u.lanaddr, localPort, 'Vifib openvpn server', ''):
+            return (u.externalipaddress(), externalPort)
 
 # TODO : specify a lease duration
