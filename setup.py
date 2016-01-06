Nexedi/re6stnet

commit 8df410f7744fd35b6584142dec3a15023cb4f6e0

Use python2 instead python and fix install for a custom Python interpreter

setup.py
@@ -1,6 +1,7 @@
 """Resilient, Scalable, IPv6 Network
 """
-
+import os, stat
+from distutils.command.build_scripts import first_line_re
 from setuptools import setup, find_packages
 from setuptools.command import sdist as _sdist, build_py as _build_py
 from distutils import log
@@ -17,6 +18,19 @@ def copy_file(self, infile, outfile, *args, **kw):
                     if not x[0].startswith("_"):
                         f.write("%s = %r\n" % x)
         return outfile, 1
+    elif isinstance(self, build_py) and \
+         os.stat(infile).st_mode & stat.S_IEXEC:
+        # Adjust interpreter of OpenVPN hooks.
+        with open(infile) as src:
+            first_line = src.readline()
+            m = first_line_re.match(first_line)
+            if m and not self.dry_run:
+                log.info("copying and adjusting %s -> %s", infile, outfile)
+                executable = self.distribution.command_obj['build'].executable
+                with open(outfile, "wb") as dst:
+                    dst.write("#!%s%s\n" % (executable, m.group(1) or ''))
+                    dst.write(src.read())
+                return outfile, 1
     cls, = self.__class__.__bases__
     return cls.copy_file(self, infile, outfile, *args, **kw)
 

Makefile
@@ -17,12 +17,14 @@ install: install-noinit
 	done
 
 install-noinit: install-man
+	install -Dp daemon/network-manager $(DESTDIR)$(NM)
 	set -e $(DESTDIR)$(PREFIX) /bin/re6stnet; [ -x $$1$$2 ] || \
-	python2.7 setup.py install --prefix=$(PREFIX) --root=$(DESTDIR); \
+	$(or $(PYTHON),python2) setup.py install \
+		--prefix=$(PREFIX) --root=$(DESTDIR); \
+	$(and $(PYTHON),sed -ri '1s:^#!\S+:#!$(PYTHON):' $(DESTDIR)$(NM);) \
 	install -d $$1/sbin; mv $$1$$2 $$1/sbin
 	install -Dpm 0644 daemon/README.conf $(DESTDIR)/etc/re6stnet/README
 	install -Dpm 0644 daemon/logrotate.conf $(DESTDIR)/etc/logrotate.d/re6stnet
-	install -Dp daemon/network-manager $(DESTDIR)$(NM)
 
 install-man: $(MANPAGELIST)
 	set -e; for x in $^; \

daemon/network-manager
@@ -1,4 +1,4 @@
-#!/usr/bin/python -S
+#!/usr/bin/python2 -S
 import errno, glob, os, signal, socket, subprocess, sys, time
 
 DAEMON = "re6stnet"

debian/common.mk
@@ -18,7 +18,8 @@ debian/changelog:
 endif
 
 override_dh_install:
-	make DESTDIR=$(TMP) PREFIX=/usr install-noinit install-ifupdown
+	make DESTDIR=$(TMP) PREFIX=/usr PYTHON=/usr/bin/python \
+		install-noinit install-ifupdown
 
 override_dh_installinit:
 	install -d $(INIT)

debian/rules
@@ -15,5 +15,8 @@ override_dh_python2:
 override_dh_auto_clean:
 	make clean
 
+# Do not build twice ('setup.py install' builds automatically)
+override_dh_auto_build:
+
 %:
 	dh $@ --with python2 --buildsystem=python_distutils

demo/demo
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 import math, nemu, os, re, signal, socket, subprocess, sys, time, weakref
 from collections import defaultdict
 IPTABLES = 'iptables'

draft/re6st-cn
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 import sqlite3, sys
 if 're6st' not in sys.modules:
     import os; sys.path[0] = os.path.dirname(sys.path[0])

draft/re6st-geo
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 import argparse, httplib,  select, socket, sqlite3, struct, sys, time, traceback
 import xml.etree.cElementTree as ET

commit 3ea6efe3e01b300d6a49a51adb62bf1e9cde2158

Fix install of OpenVPN hooks
This fixes a regression caused by commit
  8df410f7744fd35b6584142dec3a15023cb4f6e0

setup.py
@@ -27,9 +27,13 @@ def copy_file(self, infile, outfile, *args, **kw):
             if m and not self.dry_run:
                 log.info("copying and adjusting %s -> %s", infile, outfile)
                 executable = self.distribution.command_obj['build'].executable
-                with open(outfile, "wb") as dst:
-                    dst.write("#!%s%s\n" % (executable, m.group(1) or ''))
-                    dst.write(src.read())
+                patched = "#!%s%s\n" % (executable, m.group(1) or '')
+                patched += src.read()
+                dst = os.open(outfile, os.O_CREAT | os.O_WRONLY | os.O_TRUNC)
+                try:
+                    os.write(dst, patched)
+                finally:
+                    os.close(dst)
                 return outfile, 1
     cls, = self.__class__.__bases__
     return cls.copy_file(self, infile, outfile, *args, **kw)