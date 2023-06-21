46bb18dea2b3f1b546dccf2eca6bf68e847af2ff
Tom Niget <tom.niget@nexedi.com>
2023-06-21 11:26:37 +0200

    py2to3: migrate to python3
    style: stop using deprecated getargspec for rpc
    
    
    py2to3: migrate setup.py
    
    
    style: remove new-style class code from the Python2 days
    
    
    demo: properly escape screen command parameters
    
    
    
    bug: fix wrong sending of HMAC header causing test failure
    
    
    py2to3: revert changes from 90a624f in debian/control and re6stnet.spec
    
    setup: indicate minimum Python versions
    
    doc: update dependencies in readme
    
    
    style: revert pep8 changes
    
    Fix permission for ovpn-client and ovpn-server
    
    vcs: add test-related generated files to gitignore

--- demo/fixnemu.py
+++ demo/fixnemu.py
@@ -18,10 +18,10 @@
 
 import re
 import os
-from new import function
 from nemu.iproute import backticks, get_if_data, route, \
     get_addr_data, get_all_route_data, interface
 from nemu.interface import Switch, Interface
+from types import FunctionType
 
 def _get_all_route_data():
     ipdata = backticks([IP_PATH, "-o", "route", "list"]) # "table", "all"
@@ -56,7 +56,7 @@ def _get_all_route_data():
             metric))
     return ret
 
-get_all_route_data.func_code = _get_all_route_data.func_code
+get_all_route_data.__code__ = _get_all_route_data.__code__
 
 interface__init__ = interface.__init__
 def __init__(self, *args, **kw):
@@ -65,12 +65,12 @@ def __init__(self, *args, **kw):
         self.name = self.name.split('@',1)[0]
 interface.__init__ = __init__
 
-get_addr_data.orig = function(get_addr_data.func_code,
-                              get_addr_data.func_globals)
+get_addr_data.orig = FunctionType(get_addr_data.__code__,
+                              get_addr_data.__globals__)
 def _get_addr_data():
     byidx, bynam = get_addr_data.orig()
-    return byidx, {name.split('@',1)[0]: a for name, a in bynam.iteritems()}
-get_addr_data.func_code = _get_addr_data.func_code
+    return byidx, {name.split('@',1)[0]: a for name, a in bynam.items()}
+get_addr_data.__code__ = _get_addr_data.__code__
 
 @staticmethod
 def _gen_if_name():
