eada25c35129a7d8750e398f16b5de7f4fe5a8ad
Tom Niget <tom.niget@nexedi.com>
2023-06-21 11:26:37 +0200

    py2to3: migrate to python3
    style: stop using deprecated getargspec for rpc
    
    
    py2to3: migrate setup.py
    
    
    style: remove new-style class code from the Python2 days
    
    
    demo: properly escape screen command parameters
    
    
    
    bug: fix wrong sending of HMAC header causing test failure
    
    
    py2to3: revert changes from 90a624f in debian/control and re6stnet.spec
    
    setup: indicate minimum Python versions
    
    doc: update dependencies in readme
    
    
    style: revert pep8 changes

--- re6st/upnpigd.py
+++ re6st/upnpigd.py
@@ -7,7 +7,7 @@ class UPnPException(Exception):
     pass
 
 
-class Forwarder(object):
+class Forwarder:
     """
     External port is chosen randomly between 32768 & 49151 included.
     """
@@ -40,7 +40,7 @@ class Forwarder(object):
         def wrapper(*args, **kw):
             try:
                 return wrapped(*args, **kw)
-            except Exception, e:
+            except Exception as e:
                 raise UPnPException(str(e))
         return wraps(wrapped)(wrapper)
 
@@ -68,14 +68,14 @@ class Forwarder(object):
         else:
             try:
                 return self._refresh()
-            except UPnPException, e:
-                logging.debug("UPnP failure", exc_info=1)
+            except UPnPException as e:
+                logging.debug("UPnP failure", exc_info=True)
                 self.clear()
         try:
             self.discover()
             self.selectigd()
             return self._refresh()
-        except UPnPException, e:
+        except UPnPException as e:
             self.next_refresh = self._next_retry = time.time() + 60
             logging.info(str(e))
             self.clear()
@@ -109,7 +109,7 @@ class Forwarder(object):
                 try:
                     self.addportmapping(port, *args)
                     break
-                except UPnPException, e:
+                except UPnPException as e:
                     if str(e) != 'ConflictInMappingEntry':
                         raise
                     port = None
