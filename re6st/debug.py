785df54cf06157fedd21574ba6883be5bb2585dc
Tom Niget <tom.niget@nexedi.com>
2024-05-16 11:07:08 +0200

    style: remove new-style class code from the Python2 days

--- re6st/debug.py
+++ re6st/debug.py
@@ -1,7 +1,7 @@
 import errno, os, socket, stat, threading
 
 
-class Socket(object):
+class Socket:
 
     def __init__(self, socket):
         # In case that the default timeout is not None.
@@ -45,7 +45,7 @@ class Socket(object):
         return False
 
 
-class Console(object):
+class Console:
 
     def __init__(self, path, pdb):
         self.path = path
