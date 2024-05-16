5cb13425c77128002af31a707d1377898cf25c0e
Tom Niget <tom.niget@nexedi.com>
2024-05-16 16:03:24 +0200

    Fix demo by removing certs first

--- demo/clean.sh
+++ demo/clean.sh
@@ -4,3 +4,5 @@ for b in $(sudo ip l | grep -Po 'NETNS\w\w[\d\-a-f]+'); do sudo ip l del $b; don
 pkill screen
 killall python
 killall python3
+find . -name '*.db' -delete
+find . -name '*.crt' -delete
