e2607bea9844facb902160251ed471f9ca15ee63
Tom Niget <tom.niget@nexedi.com>
2024-06-09 22:32:49 +0000

    tests: cleanup test folder in teardown
    Same as demo, tests expect a clean work directory, otherwise weird behavior will occur.

--- re6st/tests/test_network/re6st_wrap.py
+++ re6st/tests/test_network/re6st_wrap.py
@@ -29,8 +29,9 @@ RE6ST_CONF = PYTHON + " -m re6st.cli.conf"
 
 def initial():
     """create the workplace"""
-    if not WORK_DIR.exists():
-        WORK_DIR.mkdir()
+    if WORK_DIR.exists():
+        shutil.rmtree(str(WORK_DIR))
+    WORK_DIR.mkdir()
 
 def ip_to_serial(ip6):
     """convert ipv6 address to serial"""
