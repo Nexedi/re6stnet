f49c80b80d4ad6ebad99959473f6b0121945f5c6
Thomas Gambier <thomas.gambier@nexedi.com>
2024-10-10 12:26:08 +0200

    various fixup for python3

--- re6st/utils.py
+++ re6st/utils.py
@@ -8,14 +8,7 @@ HMAC_LEN = len(hashlib.sha1(b'').digest())
 class ReexecException(Exception):
     pass
 
-try:
-    subprocess.CalledProcessError(0, '', '')
-except TypeError: # BBB: Python < 2.7
-    def __init__(self, returncode, cmd, output=None):
-        self.returncode = returncode
-        self.cmd = cmd
-        self.output = output
-    subprocess.CalledProcessError.__init__ = __init__
+subprocess.CalledProcessError(0, '', '')
 
 logging_levels = logging.WARNING, logging.INFO, logging.DEBUG, 5
 
@@ -174,7 +167,7 @@ class Popen(subprocess.Popen):
             self.terminate()
             t = threading.Timer(5, self.kill)
             t.start()
-            r = os.waitid(os.P_PID, self.pid, os.WNOWAIT)
+            r = os.waitid(os.P_PID, self.pid, os.WEXITED or os.WNOWAIT)
             t.cancel()
             self.poll()
             return r
