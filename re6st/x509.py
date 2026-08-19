f08c6ca86d8ffcf56e46839eccbe96feae1cefd7
Julien Muchembled <jm@nexedi.com>
2026-08-19 18:43:40 +0200

    Add support for older versions of cryptography

--- re6st/x509.py
+++ re6st/x509.py
@@ -7,9 +7,16 @@ from OpenSSL import crypto
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.x509 import \
-    load_der_x509_certificate, load_pem_x509_certificate
+try: # BBB: old cryptography
+    from cryptography.hazmat.backends.openssl.backend import backend
+    load_pem_private_key =  backend.load_pem_private_key
+except AttributeError:
+    from cryptography.hazmat.primitives.serialization import load_pem_private_key
+    from cryptography.x509 import \
+        load_der_x509_certificate, load_pem_x509_certificate
+else:
+    load_der_x509_certificate = backend.load_der_x509_certificate
+    load_pem_x509_certificate = backend.load_pem_x509_certificate
 
 from . import utils
 from .version import protocol
